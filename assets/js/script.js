4f87193de4a6eea3c2b4a1840463553ca0482bf3
Nozugo <msatriokeren@gmail.com>
2023-06-17 22:27:01 +0700

    UPDATE

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function hamburgerFunc() {
+    var sidebar = document.getElementById("sidebar");
+    var sidebarStyle = getComputedStyle(sidebar);
+
+    if (sidebarStyle.display === "none") {
+        sidebar.style.display = "block";
+    } else {
+        sidebar.style.display = "none";
+    }
+
+    return false;
+}
+
+function disableHamburgerFunc(x) {
+    var sidebar = document.getElementById("sidebar");
+
+    console.log('berhasil');
+    console.log(sidebar);
+
+    if (x.matches) {
+        sidebar.style.display = "none";
+    } else {
+        sidebar.style.display = "block";
+    }
+}
+
+var size = window.matchMedia("(min-width: 570px)");
+disableHamburgerFunc(size);
+size.addEventListener("change", disableHamburgerFunc);
