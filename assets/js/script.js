2e7b1fabdc5729c0272543d46efdafbfcd3abebb
Nozugo <msatriokeren@gmail.com>
2023-06-19 08:23:32 +0700

    UPDATE

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
 function hamburgerFunc() {
     var sidebar = document.getElementById("sidebar");
+    var hamburger = document.getElementById("hamburger-menu-container");
     var sidebarStyle = getComputedStyle(sidebar);
 
     if (sidebarStyle.display === "none") {
         sidebar.style.display = "block";
+        hamburger.style.backgroundColor = "rgb(48, 38, 69)";
+
     } else {
         sidebar.style.display = "none";
+        hamburger.style.backgroundColor = "transparent";
     }
 
-    return false;
+    return false;    
 }
 
 function disableHamburgerFunc(x) {
